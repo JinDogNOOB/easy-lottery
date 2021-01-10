9faece2d2a02f08001a6981385cbcc9a7e07623c
JinDogNOOB <mrriey00@naver.com>
2021-01-10 17:06:03 +0900

    mod name of useWeb3Setter to connector

--- src/components/WalletSelector/WalletSelector.tsx
+++ src/components/WalletSelector/WalletSelector.tsx
@@ -1,19 +1,19 @@
 import React, {useEffect} from 'react';
 import useWeb3 from '../../hooks/useWeb3';
-import useWeb3Setter from '../../hooks/useWeb3Setter';
+import useWeb3Connector from '../../hooks/useWeb3Connector';
 
 import Button from '../Button';
 
 const WalletSelector: React.FC = () => {
     const web3 = useWeb3();
-    const onSetWeb3 = useWeb3Setter();
+    const onConnectWeb3 = useWeb3Connector();
     
     return (
         <div>
             {web3?(
                 <Button buttonText="Connected" />
             ):(
-                <Button buttonText="Connect Wallet" onClick={onSetWeb3} />
+                <Button buttonText="Connect Wallet" onClick={onConnectWeb3} />
             )}
         </div>
         
--- src/hooks/useWeb3Connector.ts
+++ src/hooks/useWeb3Connector.ts
@@ -2,9 +2,9 @@ import {useContext} from 'react';
 import {Context} from '../contexts/Web3Provider';
 
 
-const useWeb3Setter = () => {
+const useWeb3Connector = () => {
     const {onSetWeb3} = useContext(Context);
     return onSetWeb3;
 }
 
-export default useWeb3Setter;
+export default useWeb3Connector;

7cd38aa4602c4a3a3aba6f402f961bbc8576d715
JinDogNOOB <mrriey00@naver.com>
2021-01-11 16:57:46 +0900

    add Home view

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Page from '../../components/Page';
+import PageHeader from '../../components/PageHeader';
+
+import mainLogo from '../../assets/image/mainLogo.png';
+
+
+const Home : React.FC = () => {
+    
+    return (
+        <Page>
+            <PageHeader 
+                icon={mainLogo}
+                title="easy lottery"
+                subtitle="easy lottery makes lottery more fair"
+            />
+
+
+
+
+
+        </Page>
+    )
+}
+
+export default Home;

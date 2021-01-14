abb14b97fab0969d18cc62a1a3ba29b14dd52119
JinDogNOOB <mrriey00@naver.com>
2021-01-14 13:41:33 +0900

    add palying function on GameComponents and works well

--- src/views/Lottery/components/Game.tsx
+++ src/views/Lottery/components/Game.tsx
@@ -8,11 +8,37 @@ import useWallet from '../../../hooks/useWallet';
 
 
 const Game : React.FC = () => {
-    const {address, balance} = useWallet();
+    const [drawNumber, setDrawNumber] = useState<number>(1);
     const blockNumber = useBlockNumber();
     const ELvariables = useEasyLotteryVarialbes();
     const gameStatus = useGameStatus();
+    const contract = useEasyLotteryContract();
 
+    const onSetDrawNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let temp = Number(e.target.value);
+        if (Number.isNaN(temp)){
+            alert("1~100 숫자만 입력하세요");
+        }else{
+            setDrawNumber(Number(e.target.value)%100);
+        }
+    }
+    
+    const onEnterDraw = async(number: number) => {
+        let result = await contract?.passNumberNPay(number);
+        if(result == true) alert(number + "번 응모 성공");
+        else alert("응모 실패")
+    }
+    const onDraw = async() => {
+        let result = await contract?.draw();
+        if(result == true) alert("추첨 성공");
+        else alert("추첨 실패");
+    }
+    const onWithdrawDevWei = async() => {
+        let result = await contract?.withdrawDevWei();
+        if (result == true) alert("개발비출금성공");
+        else alert("출금 실패");
+    }
+    
     return(
         <StyledGame>
             <StyledGameElements>
@@ -22,9 +48,10 @@ const Game : React.FC = () => {
                 <li>Gathered Wei for Dev: {ELvariables.weiForDev.toString()}</li>
             </StyledGameElements>
             <StyledGameElements>
-                <li></li>
-                <li>draw</li>
-                <li>withdrawDevWei</li>
+                <li><input type="number" value={drawNumber} onChange={onSetDrawNumber}/></li>
+                <li><input type="button" value="pay" onClick={() => onEnterDraw(drawNumber)}/></li>
+                <li><input type="button" value="draw" onClick={() => onDraw()}/></li>
+                <li><input type="button" value="withdrawDevWei" onClick={() => onWithdrawDevWei()}/></li>
                   
             </StyledGameElements>
 

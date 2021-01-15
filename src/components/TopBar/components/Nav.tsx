1c3534bf54696c217b7088d6620566516ccfa956
JinDogNOOB <mrriey00@naver.com>
2021-01-15 16:01:15 +0900

    mod ui on TopBar

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
-import styled from 'styled-components';
-import {NavLink, useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      
+    },
+    menuButton: {
+      marginRight:"10%",
+    },
+    text: {
+        background: "#1976d2",
+      color: "white",
+      fontSize: "1.2rem",
+    },
+
+  }),
+);
 
 
 const Nav: React.FC = () => {
     const history = useHistory();
+    const classes = useStyles();
 
     return (
-        <ButtonGroup size="large">
-            <Button onClick={() => {history.push('/')}}>Home</Button>
-            <Button onClick={() => {history.push('/lottery')}}>Lottery</Button>
-            <Button onClick={() => {history.push('/wallet')}}>Wallet</Button>
-            <Button onClick={() => {history.push('/law')}}>Law</Button>
+
+        <ButtonGroup  size="large" aria-label="small outlined button group">
+            <Button className={classes.text} onClick={() => { history.push('/') }}>Home</Button>
+            <Button className={classes.text} onClick={() => { history.push('/lottery') }}>Lottery</Button>
+            <Button className={classes.text} onClick={() => { history.push('/wallet') }}>Wallet</Button>
+            <Button className={classes.text} onClick={() => { history.push('/law') }}>Law</Button>
         </ButtonGroup>
     )
 }
